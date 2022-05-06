b0bcccd448971d33c00850be57d6a6776122daf1
danlhennessy <danh118@hotmail.co.uk>
2022-05-07 00:22:02 +0100

    vcount3

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
-});
-
-const apiGateway = 'https://visitorcountapi.azurewebsites.net/api/HttpTrigger1?code=sZrhu0k7XIQWcu03ehWAUmZjpeP6fPUsNURWA8aMeBnNI5nORrvtww=='; 
-
-const getVisitCount = () => {
-    let count = 0;
-    fetch(apiGateway, {
-        mode: 'cors',
-    })
-    .then(response => {
+  });
+  
+  
+  const functionApi = 'https://visitorcountapi.azurewebsites.net/api/HttpTrigger1?code=sZrhu0k7XIQWcu03ehWAUmZjpeP6fPUsNURWA8aMeBnNI5nORrvtww==';
+  
+  const getVisitCount = () => {
+    let count = 7;
+    fetch(functionApi)
+      .then(response => {
         return response.json()
-    })
-    .then(res => {
-        const count = res.Attributes.visitcount;
-        document.getElementById('counter').innerText =count;
-        document.getElementById('visitorElem').style.display = 'block';
-    })
+      })
+      .then(response => {
+        count = response;
+        console.log("Hello 👋, you are visitor number - " + count);
+        document.getElementById('counter').innerText = count;
+      }).catch(function (error) {
+        console.log(error);
+      });
     return count;
-}
+  }
