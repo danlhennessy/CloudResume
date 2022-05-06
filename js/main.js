4a87d7119b761e72222714fdb10e0db3e0f50415
danlhennessy <danh118@hotmail.co.uk>
2022-05-07 00:55:00 +0100

    vcount4

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount();
-  });
-  
-  
-  const functionApi = 'https://visitorcountapi.azurewebsites.net/api/HttpTrigger1?code=sZrhu0k7XIQWcu03ehWAUmZjpeP6fPUsNURWA8aMeBnNI5nORrvtww==';
-  
-  const getVisitCount = () => {
-    let count = 7;
-    fetch(functionApi)
-      .then(response => {
-        return response.json()
-      })
-      .then(response => {
-        count = response;
-        console.log("Hello 👋, you are visitor number - " + count);
-        document.getElementById('counter').innerText = count;
-      }).catch(function (error) {
-        console.log(error);
-      });
-    return count;
-  }
+  getVisitCount();
+});
+
+
+const localApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = 'https://visitorcountapi.azurewebsites.net/api/HttpTrigger1?code=sZrhu0k7XIQWcu03ehWAUmZjpeP6fPUsNURWA8aMeBnNI5nORrvtww=='; 
+
+const getVisitCount = () => {
+  let count = 30;
+  fetch(functionApi)
+  .then(response => {
+      return response.json()
+  })
+  .then(response => {
+      console.log("Website called function API.");
+      count = response; <br />document.getElementById('counter').innerText = count;
+      document.getElementById('counter').innerText = count;
+  }).catch(function(error) {
+      console.log(error);
+    });
+  return count;
+}
